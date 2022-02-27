27cf242d96ece0e016272c61dbdb7ecd3ad80b74
AigarJ <aigar134@gmail.com>
2022-02-27 13:08:30 +0200

    [add] - QuestionRepository added

---
+++ src/main/java/project/repository/QuestionRepository.java
@@ -0,0 +1,9 @@
+package project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import project.entity.UserEntity;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<UserEntity, Long> {
+}
